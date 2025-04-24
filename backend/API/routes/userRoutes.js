da72032432f17e0dbde7dc45148ae0475a865a17
Shabbir <shabbrang90@gmail.com>
2025-04-25 02:15:48 +0530

    Added delete ques

--- backend/API/routes/userRoutes.js
+++ backend/API/routes/userRoutes.js
@@ -9,6 +9,7 @@ const {
   addQuestionnaireEntry,
   getMyQuestionnaire,
   getMyCidValues,
+  deleteQuestionnaireEntry,
 } = require("../controllers/userController");
 const auth = require("../middleware/authMiddleware");
 
@@ -23,5 +24,6 @@ router.get("/check-cid", auth, checkCid);
 router.post("/questionnaire", auth, addQuestionnaireEntry);
 router.get("/questionnaire", auth, getMyQuestionnaire);
 router.get("/all-cids", auth, getMyCidValues);
+router.delete("/questionnaire", auth, deleteQuestionnaireEntry);
 
 module.exports = router;
